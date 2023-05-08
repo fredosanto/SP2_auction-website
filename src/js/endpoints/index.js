311a8916179ff78286f7766f9bac79ff00bf1bff
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-05-08 13:29:13 +0200

    Adds page for single listing

---
+++ src/js/endpoints/index.js
@@ -0,0 +1,40 @@
+const base = "https://api.noroff.dev/api/v1/auction";
+
+export const API = {
+  auth: { register: base + "/register", login: base + "/login" },
+  profiles: {
+    $: base + "/profiles",
+    name: (name) => {
+      const nameBase = base + "/profiles/" + name;
+      return {
+        $: nameBase,
+        media: nameBase + "/media",
+        listings: nameBase + "/listings",
+        bids: nameBase + "/bids",
+        credits: nameBase + "/credits",
+      };
+    },
+  },
+  listings: {
+    /** GET - This endpoint returns a single listing. */
+    $: base + "/listings",
+    id: (id) => {
+      const listingsBase = base + "/listings/" + id;
+      return {
+        /**
+         * Signle Entry
+         *
+         * [GET] - This endpoint returns a single listing
+         *
+         * [POST] - This endpoint allows for the creation of a new listing. The title and endsAt properties are required, but we recommend including the description and media properties as well. */
+        $: listingsBase,
+        /** Supports POST */
+        bids: listingsBase + "/bids",
+      };
+    },
+  },
+};
+
+console.log(API.profiles.$);
+
+console.log(API.listings.id(4).$);
